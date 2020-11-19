0c6a8001b87fa0d7cab9256c58aba1b91c8376c2
addussk <34662199+addussk@users.noreply.github.com>
2020-11-19 21:24:58 +0100

    Added method to UI class responsible for deleteBook after click button, method showAlertvalidation process

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ class Book {
       this.isbn = isbn;
     }
 }
+
 // UI Class: Handle UI Taska
 class UI {
     // Method resposible for display list of books
@@ -45,7 +46,25 @@ class UI {
         list.appendChild(row);
     }
 
-    static clearFields(){
+    static deleteBook(el) {
+        if(el.classList.contains('delete')) {
+          el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(msg, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(msg));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+    
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
+    static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
         document.querySelector('#isbn').value = '';
@@ -60,18 +79,37 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 document.querySelector('#book-form').addEventListener('submit', (e) => {
     // Prevent actial submit
     e.preventDefault();
+
     // Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instatiate book
-    const book = new Book(title, author, isbn);
+    // Validate
+    if(title === '' || author === '' || isbn === '')
+    {
+        UI.showAlert("Please fill in all fields", 'danger');
+    }else{
+        // Instatiate book
+        const book = new Book(title, author, isbn);
 
-    // Add book to list
-    UI.addBookToList(book);
+        // Add book to list
+        UI.addBookToList(book);
 
-    // Clear fields
-    UI.clearFields();
-})
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }
+});
+
 // Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // console.log(e.target)
+    
+    UI.deleteBook(e.target);
+
+    // Show success message
+    UI.showAlert('Book Removed', 'success');
+});
